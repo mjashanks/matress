822e5955bb00bf35a6101ce5a872ce18a19ce702
Mike <michael.shanks@blue-zinc.com>
2017-04-18 16:22:50 +0100

    dont know what im committeing here

--- src/account-overview.ts
+++ src/account-overview.ts
@@ -1,14 +1,30 @@
+import *  as _ from "lodash"
+
 export class AccountOverview {
     balance = 36.4455
-    transactions = [
+    allTransactions = [
         {Date : "2017-01-12", 
+        Type: "Credit",
         Description : "thanks for the cheese",
         Amount : 2.44},
         {Date : "2017-02-03", 
         Description : "from mike, enjoy!",
+        Type: "Credit",
         Amount : 5.112},
         {Date : "2017-02-06", 
-        Description : "from acme Ltd",
-        Amount : 5.87},
+        Description : "for that thing you did",
+        Type: "Debit",
+        Amount : -5.87},
+        {Date : "2017-02-06", 
+        Description : "Transaction",
+        Type: "Gas",
+        Amount : -0.00042}
     ];
+    transactions = this.allTransactions;
+
+    FilterType(typ : string){
+        this.transactions = _.filter(this.allTransactions, t => {
+            return t.Type === typ;
+        })
+    }
 }
